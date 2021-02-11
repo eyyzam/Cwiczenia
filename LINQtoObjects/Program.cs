fabb9b09307ad437ea1ab670674bacf03ff9c381
eyyzam <bronislawskiandrzej@gmail.com>
2021-02-11 21:33:14 +0100

    Exercise  "LINQ and Objets" done

--- LINQtoObjects/Program.cs
+++ LINQtoObjects/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace LINQtoObjects
 {
@@ -6,7 +7,26 @@ namespace LINQtoObjects
 	{
 		internal static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			string input;
+
+			while (true)
+			{
+				Console.Write("Podaj sekwencję cięć materiału: ");
+				input = Console.ReadLine();
+
+				if (string.IsNullOrEmpty(input))
+					Console.WriteLine("Niepoprawna wartość");
+				else break;
+			}
+
+			var result = input.Split('-')
+							  .Select((row, i) => (row.Contains(';') ? row : i == 0 ? (row == "0:00:00" ? "" : $"0:00:00;{row}") : (row == "2:00:00" ? "" : $"{row};2:00:00"))
+							  .Replace(";", "-"))
+							  .Where(row => row.Any())
+							  .Aggregate((left, right) => $"{left};{right}");
+
+			Console.WriteLine(string.IsNullOrEmpty(result) ? "\nempty" : $"\n{result}");
+			Console.ReadKey();
 		}
 	}
 }
