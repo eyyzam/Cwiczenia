80a663b76941356b47d178e6cb579cd03b24961b
eyyzam <bronislawskiandrzej@gmail.com>
2021-02-12 21:35:57 +0100

    Added immutability for properties in Time struct

--- TimeAndTimePeriod/Models/Structures/Time.cs
+++ TimeAndTimePeriod/Models/Structures/Time.cs
@@ -1,15 +1,34 @@
 ﻿// ReSharper disable once CheckNamespace
+
+using System;
+
 public struct Time
 {
 	private int _miliseconds;
 	private byte _seconds, _minutes, _hours;
 
-	public int Miliseconds => _miliseconds;
+	public int Miliseconds
+	{
+		get => _miliseconds;
+		private set => _miliseconds = value % 1000;
+	}
 
-	public byte Seconds => _seconds;
+	public byte Seconds
+	{
+		get => _seconds;
+		private set => _seconds = Convert.ToByte(value % 60);
+	}
 
-	public byte Minutes => _minutes;
+	public byte Minutes
+	{
+		get => _minutes;
+		private set => _minutes = Convert.ToByte(value % 60);
+	}
 
-	public byte Hours => _hours;
+	public byte Hours
+	{
+		get => _hours;
+		private set => _hours = Convert.ToByte(value % 24);
+	}
 }
 
