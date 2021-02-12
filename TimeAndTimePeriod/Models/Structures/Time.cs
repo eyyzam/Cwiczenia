4e30378de7378003f302deb0f88c41ad93dc8d71
eyyzam <bronislawskiandrzej@gmail.com>
2021-02-12 21:34:08 +0100

    Added properties for Time struct

---
+++ TimeAndTimePeriod/Models/Structures/Time.cs
@@ -0,0 +1,15 @@
+﻿// ReSharper disable once CheckNamespace
+public struct Time
+{
+	private int _miliseconds;
+	private byte _seconds, _minutes, _hours;
+
+	public int Miliseconds => _miliseconds;
+
+	public byte Seconds => _seconds;
+
+	public byte Minutes => _minutes;
+
+	public byte Hours => _hours;
+}
+
